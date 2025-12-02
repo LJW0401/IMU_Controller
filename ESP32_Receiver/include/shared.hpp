3e853a94885dfee3d6826055c03781cc055e5a64
是小企鹅呀 <1357482552@qq.com>
2025-12-02 22:17:45 +0800

    创建控制任务

--- ESP32_Receiver/include/shared.hpp
+++ ESP32_Receiver/include/shared.hpp
@@ -4,6 +4,7 @@
 #include "protocol_wifi.hpp"
 
 // task_host.cpp
-extern protocol_wifi::imu_u imu_data;
+extern protocol_wifi::imu_u head_imu_data;
+extern protocol_wifi::imu_u pose_imu_data;
 
 #endif  // SHARED_HPP
--- ESP32_Receiver/src/main.cpp
+++ ESP32_Receiver/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 
 #include "task_host.hpp"
+#include "task_state_control.hpp"
 
 using namespace task_host;
 
 // 定义任务句柄
 TaskHandle_t HostTaskHandle = NULL;
+TaskHandle_t StateControlTaskHandle = NULL;
 
 uint32_t count = 0;
-
 
 void setup()
 {
@@ -17,12 +18,21 @@ void setup()
 
     // 创建任务
     xTaskCreate(
-        HostTask,          // 任务函数
-        "host_task",        // 任务名称
-        4096,              // 任务栈大小（字节）
-        NULL,              // 任务参数
-        1,                 // 任务优先级
+        HostTask,        // 任务函数
+        "HostTask",      // 任务名称
+        4096,            // 任务栈大小（字节）
+        NULL,            // 任务参数
+        1,               // 任务优先级
         &HostTaskHandle  // 任务句柄
+    );
+
+    xTaskCreate(
+        StateControlTask,        // 任务函数
+        "StateControlTask",      // 任务名称
+        4096,                    // 任务栈大小（字节）
+        NULL,                    // 任务参数
+        1,                       // 任务优先级
+        &StateControlTaskHandle  // 任务句柄
     );
 
     Serial.println("初始化完成");
